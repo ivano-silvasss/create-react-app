30f673da4db6f44eba8eb9095d5e8a11df86f262
VIADO <ivano.silvasss@gmail.com>
2023-04-09 09:39:04 -0300

    test3

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
       <>
 
           <h1> teste </h1>
-          <div style="text-align:center;" id="96789-31"><script src="//ads.themoneytizer.com/s/gen.js?type=31"></script><script src="//ads.themoneytizer.com/s/requestform.js?siteId=96789&formatId=31"></script></div>
-          </>
+
+      </>
   );
 }
 
